72da9ee417e1b41c0b6da133470ad1ae335b6d78
G-M-PH <GiaminhPham@outlook.com>
2026-01-14 11:23:52 +0100

    existing account check toegevoegd
    in function registerCheck()
    staat er nu wanneer een account al bestaat

--- php/forms.php
+++ php/forms.php
@@ -11,14 +11,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['newUsername'])) {
-        if(validateUsername($_POST['newUsername']) == true)
-        {
-        $value1 = $_POST['newUsername'];
-        $value2 = $_POST['newEmail'];
-        $value3 = $_POST['newPassword'];
-        $value4 = $_POST['confirmNewPassword'];
-        registerCheck($value1, $value2, $value3, $value4);
-        } else {echo "use a normal username";}
+        if (validateUsername($_POST['newUsername']) == true) {
+            $value1 = $_POST['newUsername'];
+            $value2 = $_POST['newEmail'];
+            $value3 = $_POST['newPassword'];
+            $value4 = $_POST['confirmNewPassword'];
+            registerCheck($value1, $value2, $value3, $value4);
+            echo"W username";
+        } else {
+            echo "L username";
+        }
     } else {
         $error = 'Invalid info';
     }
--- php/functions.php
+++ php/functions.php
@@ -24,11 +24,29 @@ function loginSucess($id, $username)
 }
 function validateUsername($username)
 {
-    return ("false");
-    echo"successfull function call";
+    return preg_match('/^[a-zA-Z0-9_]{3,24}$/', $username);
 }
-function registerCheck($username, $email, $password, $password2){
+function registerCheck($username, $email, $password, $password2)
+{
+    if ($username && $email && ($password == $password2)) {
+        global $conn;
+        $stmt = $conn->prepare('SELECT user_id, username, email FROM users WHERE username = ? OR email = ? ');
+        $stmt->execute([$username, $email]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC); 
+        if ($user === false) {
+            echo "geen user dus maak account";
+        } else {
+            print_r($user);
+            echo "er is user geen account maken pls";
+        }
+
+        createAccount($username, $email, $password);
+    } else {
+        echo "password no same you bum";
+    }
+}
+function createAccount($username, $email, $password)
+{
 
 }
-
 ?>
